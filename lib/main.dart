7d05fbb567b602a7ab3584a6a5916b933f9057c5
Suyash <suyashgupta374@gmail.com>
2023-06-22 16:34:08 +0530

    Added Functionality of buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
-import 'package:chat/services/login_or_signup.dart';
+import 'package:chat/services/auth_gate.dart';
+import 'package:chat/services/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthService(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,10 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginOrSignUp()
-    );
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Auth());
   }
 }
-
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:chat/components/sign_button.dart';
 import 'package:chat/components/text_field.dart';
+import 'package:chat/services/auth_service.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   final void Function()? onTap;
@@ -15,6 +17,24 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  //user sign in
+  void signIn() async {
+    //get the auth service
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    try {
+      await authService.signInWithEmailAndPassword(
+          emailController.text, passwordController.text);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,71 +43,74 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-                //logo
-                const FaIcon(
-                  FontAwesomeIcons.solidComments,
-                  size: 100,
-                  color: Colors.blueGrey,
-                ),
+                  //logo
+                  const FaIcon(
+                    FontAwesomeIcons.solidComments,
+                    size: 100,
+                    color: Colors.blueGrey,
+                  ),
 
-                const SizedBox(
-                  height: 50,
-                ),
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-                //welcome back message
-                const Text(
-                  'Welcome back :)',
-                  style: TextStyle(fontSize: 24),
-                ),
+                  //welcome back message
+                  const Text(
+                    'Welcome back :)',
+                    style: TextStyle(fontSize: 24),
+                  ),
 
-                const SizedBox(
-                  height: 25,
-                ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-                // email text field
-                MyTextField(
-                    controller: emailController,
-                    hintText: 'Email',
-                    obscureText: false),
+                  // email text field
+                  MyTextField(
+                      controller: emailController,
+                      hintText: 'Email',
+                      obscureText: false),
 
-                const SizedBox(
-                  height: 15,
-                ),
+                  const SizedBox(
+                    height: 15,
+                  ),
 
-                // password text field
-                MyTextField(
-                    controller: passwordController,
-                    hintText: 'Password',
-                    obscureText: true),
+                  // password text field
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: true),
 
-                const SizedBox(
-                  height: 25,
-                ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-                //sign-in button
-                SignButton(onTap: () {}, text: 'Sign in'),
-                const SizedBox(
-                  height: 50,
-                ),
-                // Redirect to Sign up screen
-                Text.rich(
-                  style: const TextStyle(fontSize: 17),
-                  TextSpan(text: "Don't have an account? ", children: [
-                    TextSpan(
-                      text: 'SignUP',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                      recognizer: TapGestureRecognizer()..onTap = widget.onTap,
-                    ),
-                  ]),
-                )
-              ],
+                  //sign-in button
+                  SignButton(onTap: signIn, text: 'Sign in'),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  // Redirect to Sign up screen
+                  Text.rich(
+                    style: const TextStyle(fontSize: 17),
+                    TextSpan(text: "Don't have an account? ", children: [
+                      TextSpan(
+                        text: 'Sign Up',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onTap,
+                      ),
+                    ]),
+                  )
+                ],
+              ),
             ),
           ),
         ),
